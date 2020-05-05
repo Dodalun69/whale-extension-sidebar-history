0e94b01f78dc640defba7d718ad148b50cd0358c
MATE <mate131909@gmail.com>
2020-05-06 04:40:24 +0900

    시간 구분선 추가

--- src/sidebarPage/components/History/HistoryList.tsx
+++ src/sidebarPage/components/History/HistoryList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import HistoryItem from "./HistoryItem";
 
 import "./HistoryList.scss";
@@ -6,59 +6,82 @@ import "./HistoryList.scss";
 type HistoryListProps = {
   historys: whale.history.HistoryItem[];
   fallbackMessage: string;
+  timeLineInfo?: {
+    baseDay: number;
+  };
 };
 
-function HistoryList({ historys, fallbackMessage }: HistoryListProps) {
-  // 방문 기록 데이터가 비어있을 경우 처리
+function TimeLineHistory({
+  timeLineInfo,
+  historys,
+}: {
+  timeLineInfo: {
+    baseDay: number;
+  };
+  historys: whale.history.HistoryItem[];
+}) {
+  const timeFlag = {
+    hour: -1,
+    prevHour: -1,
+    flag: false,
+  };
+
+  function renderTimeDivider(time: number) {
+    return (
+      <div className="time-divider">
+        <hr />
+        <div>{`${time < 10 ? `0${time}` : time} : 00`}</div>
+        <hr />
+      </div>
+    );
+  }
+
+  function renderHistory(history: whale.history.HistoryItem) {
+    const { lastVisitTime } = history;
+    const time = new Date(lastVisitTime);
+
+    if (
+      time.getDate() === timeLineInfo.baseDay &&
+      timeFlag.hour !== time.getHours()
+    ) {
+      timeFlag.hour = time.getHours();
+
+      if (timeFlag.prevHour !== timeFlag.hour && timeFlag.prevHour !== -1) {
+        timeFlag.flag = true;
+      }
+    }
+
+    const result = [
+      timeFlag.flag ? renderTimeDivider(timeFlag.prevHour) : null,
+      <HistoryItem key={history.id} history={history} />,
+    ];
+
+    timeFlag.flag = false;
+    timeFlag.prevHour = timeFlag.hour;
+
+    return result;
+  }
+
+  return (
+    <div id="history-list">
+      {historys.map(history => renderHistory(history))}
+      {renderTimeDivider(timeFlag.prevHour)}
+    </div>
+  );
+}
+
+function HistoryList({
+  historys,
+  fallbackMessage,
+  timeLineInfo,
+}: HistoryListProps) {
   if (!(historys && historys.length > 0)) {
     return <div id="history-list-announcement">{fallbackMessage}</div>;
   }
 
-  // To do: 2.0.0 먼저 올리고 2.1.X에 추가하기
-  //
-  // let hour = -1;
-  // let flag = false;
-  // function renderHistory(history: whale.history.HistoryItem) {
-  //   const { lastVisitTime } = history;
-  //   const time = new Date(lastVisitTime);
-
-  //   if (hour !== time.getHours()) {
-  //     if (hour !== -1) {
-  //       flag = true;
-  //     }
-  //     hour = time.getHours();
-  //   }
-
-  //   const result = [
-  //     <HistoryItem key={history.id} history={history} />,
-  //     flag ? (
-  //       <div
-  //         style={{
-  //           display: "flex",
-  //           justifyContent: "center",
-  //           marginTop: "12px",
-  //           marginBottom: "12px",
-  //         }}
-  //       >
-  //         <hr />
-  //         <div style={{ width: "60px" }}>{hour}</div>
-  //         <hr />
-  //       </div>
-  //     ) : null,
-  //   ];
-
-  //   flag = false;
-
-  //   return result;
-  // }
-
-  // console.log("historyList", "render", historys.length);
-
-  // return (
-  //   <div id="history-list">
-  //     {historys.map((history) => renderHistory(history))}
-  //   </div>
-  // );
+  if (timeLineInfo) {
+    return <TimeLineHistory timeLineInfo={timeLineInfo} historys={historys} />;
+  }
 
   return (
     <div id="history-list">
