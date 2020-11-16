071c67774a80662f87d29a0c04950485deee9a79
MATE <mate131909@gmail.com>
2020-11-16 17:32:37 +0900

    remove announcement

--- src/sidebarPage/App.tsx
+++ src/sidebarPage/App.tsx
@@ -5,7 +5,6 @@ import "./App.scss";
 
 import * as whaleApi from "../util/whaleApi";
 
-import Announcement from "./components/Announcement";
 import SyncedTabs from "./components/SyncedTabs";
 import History from "./components/History";
 import Footer from "./components/Footer";
@@ -13,21 +12,6 @@ import Footer from "./components/Footer";
 import { Key, getStorageData } from "./storage";
 
 function App() {
-  const [isAnnouncePageOpen, setIsAnnouncePageOpen] = useState<boolean>(false);
-
-  function initial() {
-    getStorageData(Key.isAnnouncementNeverSeeAgain).catch((error: Error) => {
-      if (error.message === "undefined") {
-        // 아직 값이 지정되지 않음(신규 설치)
-        setIsAnnouncePageOpen(true);
-      }
-    });
-  }
-
-  useEffect(() => {
-    initial();
-  }, []);
-
   return (
     <div className="App">
       <header>
@@ -42,7 +26,6 @@ function App() {
         </div>
       </header>
       <main>
-        {isAnnouncePageOpen ? <Announcement /> : null}
         <SyncedTabs />
         <History />
       </main>
